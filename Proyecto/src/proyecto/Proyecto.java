562be0668454f84afc4b2da6880fd989e31ec43f
bigmarie <nikima06@gmail.com>
2024-09-27 18:54:31 -0600

    Archivo principal
    Creacion del archivo principal para usarse durante el proyecto

--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package firstclass;
+package proyecto;
 
 /**
  *
  * @author Nicole
  */
-public class FirstClass {
+public class Proyecto {
 
     /**
      * @param args the command line arguments
