e65c0a5b2f6d3f2a36b1191ceeb44f77306a982d
Nicole <100313645+bigmarie@users.noreply.github.com>
2024-11-28 20:02:18 -0600

    Consulta acceso por codigo
    - Menu principal improvisado
    - Consulta por codigo que guarda la fecha

--- Proyecto/src/proyecto/GestionAcceso.java
+++ Proyecto/src/proyecto/GestionAcceso.java
@@ -12,14 +12,52 @@ import java.text.SimpleDateFormat;
  * @author Nicole
  */
 public class GestionAcceso {
+    private GestionArchivo archivo = new GestionArchivo();
     private Accesos arregloAccesos[] = new Accesos[400];
     
-    public void ingresoQuickPass(){
+    // Consulta de ingresos por codigo
+    public String consulta(QuickPass arreglo){
         Date fechaActual = new Date();
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm");
         
         String fecha = formato.format(fechaActual);
-        System.out.println(fecha);
+        
+        if(arreglo == null){
+            for(int i = 0; i < arregloAccesos.length; i++){
+            if(arregloAccesos[i] == null){
+                Accesos acceso = new Accesos("",0,0,Condicion.RECHAZADO,fecha);
+                arregloAccesos[i] = acceso;
+                System.out.println("Codigo no existe en la lista");
+                return "RECHAZADO";
+                }
+            }
+        }
+        
+        String filial = arreglo.getFilial();
+        int placa = arreglo.getPlaca();
+        int codigo = arreglo.getCodigo();
+        Estado estado = arreglo.getEstado();  
+        
+        if(estado == Estado.INACTIVO){
+          for(int i = 0; i < arregloAccesos.length; i++){
+            if(arregloAccesos[i] == null){
+                Accesos acceso = new Accesos(filial,codigo,placa,Condicion.RECHAZADO,fecha);
+                arregloAccesos[i] = acceso;
+                System.out.println("QuickPass inactivo");
+                return "RECHAZADO";
+                }
+            }  
+        }
+        
+        for(int i = 0; i < arregloAccesos.length; i++){
+            if(arregloAccesos[i] == null){
+                Accesos acceso = new Accesos(filial,codigo,placa,Condicion.ACEPTADO,fecha);
+                arregloAccesos[i] = acceso;
+                return "ACEPTADO";
+                }
+            }
+        
+        return null;
     }
     // Fin
 }
--- Proyecto/src/proyecto/Proyecto.java
+++ Proyecto/src/proyecto/Proyecto.java
@@ -4,6 +4,7 @@
  */
 package proyecto;
 import java.io.*;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -16,6 +17,59 @@ public class Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        boolean esValido = false;
+        GestionQuickPass gestionQuickPass = new  GestionQuickPass();
+        GestionAcceso gestionAcceso = new GestionAcceso();
+        
+        while(!esValido){
+            String m = "MENU \n"
+                + "1. Usuario \n"
+                + "2. Administrador \n"
+                + "0. Salir \n"
+                + "Ingrese la opcion que desea: ";
+            String r = JOptionPane.showInputDialog(null, m);
+            
+            switch(r){
+                case "1":
+                    String sms = "";
+                    QuickPass quickPass = gestionQuickPass.getArregloQuickPassperCodigo();
+                    sms = gestionAcceso.consulta(quickPass);
+                    System.out.println(sms);
+                    break;
+                case "2":
+                    boolean esSalir = false;
+                    
+                    while(!esSalir){
+                        m = "MENU \n"
+                            + "1. Gestion QuickPass \n"
+                            + "2. Gestion Acceso \n"
+                            + "3. Reportes \n"
+                            + "0. Salir \n"
+                            + "Ingrese la opcion que desea: ";
+                        String admin = JOptionPane.showInputDialog(null, m);
+                        switch(admin){
+                            case "1":
+                                gestionQuickPass.agregarQuickPass();
+                            case "2":
+                                break;
+                            case "3":
+                                break;
+                            case "0":
+                                esSalir = true;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+                case "0":
+                    esValido = true;
+                    break;
+                default:
+                    System.out.println("Opcion no valida");
+                    break;
+            }
+        }
         
         // End
     }    
